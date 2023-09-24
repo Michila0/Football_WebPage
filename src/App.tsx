ace7b3bd7a39a2649558353be38a058a7753d73b
Michila <michila.bimsara12@gmail.com>
2023-09-24 11:27:11 +0530

    feat: not display it is bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+//import React from 'react';
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+
+
+import {Header} from "./Components/Header_footer/header.tsx";
+import {Footer} from "./Components/Header_footer/footer.tsx";
+import {Home} from "./Components/Home/index.tsx";
+
+
 function App() {
 
 
   return (
     <>
-        hi react
+        <BrowserRouter>
+            <Header/>
+
+            <Switch>
+                <Route path='/'  exact component={Home}/>
+            </Switch>
+            <Footer/>
+        </BrowserRouter>
     </>
   )
 }
