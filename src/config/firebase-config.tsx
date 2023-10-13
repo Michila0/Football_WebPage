4829b4ea9186e083d558f6d6a94572478c29159a
Michila <michila.bimsara12@gmail.com>
2023-10-13 10:15:49 +0530

    chore: connected the firestore of the matches

--- src/config/firebase-config.tsx
+++ src/config/firebase-config.tsx
@@ -1,28 +1,3 @@
-// import * as firebase from 'firebase/app';
-// import 'firebase/auth';
-//
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDfNNpeprMlgC44FuRM6tD9P9SBjkmg5E4",
-//     authDomain: "mcity-f05af.firebaseapp.com",
-//     projectId: "mcity-f05af",
-//     storageBucket: "mcity-f05af.appspot.com",
-//     messagingSenderId: "522678571401",
-//     appId: "1:522678571401:web:fd24a5a6f4766776379158",
-//     measurementId: "G-7QLKGVW2JX"
-// };
-//
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-//
-// export {
-//     firebase
-// }
-
-
-
-
-
-
 //Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import "firebase/compat/auth";
@@ -30,6 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+import 'firebase/firestore';
+//import firebase from "firebase/compat";
+import {addDoc, collection, getFirestore } from "firebase/firestore";
+
+import { cityDb } from '../temp/m-city-export.tsx';
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -44,18 +26,17 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-//export const firebaseApp = initializeApp(firebaseConfig);
-
 export const firebaseApp = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(firebaseApp);
 export const auth = getAuth(firebaseApp);
 
 
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
+//firestore Collection
+export const matchesCollection = collection(db, 'matches')
 
 
 
-
-
-
-
-
+cityDb.matches.forEach(async item => await addDoc(matchesCollection,item))
