6de49133478014a3f0c36caf482bb0ed26cff9f6
Michila <michila.bimsara12@gmail.com>
2023-10-18 23:17:11 +0530

    feat: display the matches page

--- src/Components/home/matches/Blocks.tsx
+++ src/Components/home/matches/Blocks.tsx
@@ -4,6 +4,7 @@ import { getDocs,QueryDocumentSnapshot } from "firebase/firestore";
 //import firebase from "firebase/compat";
 //import QueryDocumentSnapshot = firebase.firestore.QueryDocumentSnapshot;
 import { Slide } from 'react-awesome-reveal';
+import {Matches_Block} from "../../utils/Matches_Block.tsx";
 
 interface MatchData {
     id: string;
@@ -33,7 +34,7 @@ export const Blocks = () => {
                     away: doc.data().away,
                     resultAway: doc.data().resultAway,
                     final: doc.data().final,
-                }))
+                }));
                 setMatches(matchesData)
                 console.log(matchesData)
 
@@ -53,7 +54,7 @@ export const Blocks = () => {
                 <Slide key={match.id} className='item' triggerOnce>
                     <div>
                         <div className='wrapper'>
-                            match
+                            <Matches_Block match={match} />
                         </div>
                     </div>
                 </Slide>
